81aefeb5c6427fd5cc1d1fd9efd847dcfaa094c0
mlny <me@mlny.info>
2017-11-09 22:00:52 +0900

    20171109

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -8,7 +8,7 @@ export LD_PRELOAD=/usr/lib/libgtk3-nocsd.so.0
 exec pulseaudio &
 exec xgamma -gamma 0.8 &
 exec /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
-exec light-locker &
+# exec light-locker &
 
 # IBus
 export GTK_IM_MODULE=ibus
@@ -17,10 +17,10 @@ export QT_IM_MODULE=ibus
 exec ibus-daemon -drx &
 
 # tray apps
-exec nm-applet --sm-disable &
+# exec nm-applet --sm-disable &
 exec urxvtd -q -f -o &
-exec volumeicon &
-exec pamac-tray &
+# exec volumeicon &
+# exec pamac-tray &
 
 # Post-Desktop apps
 exec compton &
