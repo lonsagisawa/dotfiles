e0bba27111f05a3195703257bb4b446b3a61c6ed
mlny <me@mlny.info>
2017-11-20 18:08:41 +0900

    New ricing and optimization for X240

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # gtk3-nocsd
 export GTK_CSD=0
@@ -7,8 +7,9 @@ export LD_PRELOAD=/usr/lib/libgtk3-nocsd.so.0
 # Pre-Desktop daemon/settings
 exec pulseaudio &
 exec xgamma -gamma 0.8 &
+exec xfce4-power-manager &
 exec /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
-# exec light-locker &
+exec light-locker &
 
 # IBus
 export GTK_IM_MODULE=ibus
@@ -17,11 +18,12 @@ export QT_IM_MODULE=ibus
 exec ibus-daemon -drx &
 
 # tray apps
-# exec nm-applet --sm-disable &
+exec nm-applet --sm-disable &
 exec urxvtd -q -f -o &
 # exec volumeicon &
 # exec pamac-tray &
+exec ~/.dropbox-dist/dropboxd &
 
 # Post-Desktop apps
 exec compton &
-exec ~/.fehbg &
+exec nitrogen --restore &
