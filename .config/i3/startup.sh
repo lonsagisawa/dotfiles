3ff108b0756435ed2ab2b27ad44f9988eb92994b
Lon Sagisawa <me@mlny.info>
2018-07-27 21:35:36 +0900

    i3 modified for twin bar setup

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -5,23 +5,23 @@
 #export LD_PRELOAD=/usr/lib/libgtk3-nocsd.so.0
 
 # Pre-Desktop daemon/settings
-exec pulseaudio &
-exec xgamma -gamma 0.8 &
+#exec pulseaudio &
+exec xgamma -gamma 0.85 &
 exec xfce4-power-manager &
-exec /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+#exec /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 exec light-locker &
 
 # IBus
 exec ibus-daemon -drx &
 
 # tray apps
-exec nm-applet --sm-disable &
+#exec nm-applet --sm-disable &
 #exec urxvtd -q -f -o &
 # exec volumeicon &
 # exec pamac-tray &
-exec ~/.dropbox-dist/dropboxd &
-exec insync start &
+#exec ~/.dropbox-dist/dropboxd &
+#exec insync start &
 
 # Post-Desktop apps
 exec compton &
-exec nitrogen --restore &
+#exec nitrogen --restore &
