25c5210111cb7d3f6abfa6e8609fab3dc9459332
Lon Sagisawa <lon.sagisawa@outlook.jp>
2020-10-07 01:55:27 +0900

    fish: not using vi-style keybindings anymore

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,6 @@ set -x EDITOR nvim
 set -x TERM alacritty
 set -x SHELL fish
 
-# vi keybindings
-fish_vi_key_bindings
-
 # aliases
 alias vim="nvim"
 alias g="git"
@@ -20,8 +17,4 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
-
-# bobthefish
-set -g theme_nerd_fonts yes
-set -g theme_color_scheme terminal2
 
