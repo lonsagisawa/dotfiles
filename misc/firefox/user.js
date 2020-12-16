1964132d271ef65c3ad68774633326f229a6e444
Lon Sagisawa <lon@sagisawa.me>
2020-12-16 19:01:07 +0900

    mac firefox: fix random fallback font issue

--- misc/firefox/user.js
+++ misc/firefox/user.js
@@ -19,19 +19,3 @@ user_pref("svg.context-properties.content.enabled", true);
 
 // font size
 user_pref("font.size.monospace.x-western", 16);
-
-// Typography for Windows
-user_pref("gfx.content.azure.backends", "direct2d1.1,cairo");
-user_pref("gfx.direct2d.disabled", false);
-user_pref("gfx.direct2d.force-enabled", true);
-user_pref("gfx.font_rendering.cleartype.always_use_for_content", true);
-user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);
-user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 200);
-user_pref("gfx.font_rendering.cleartype_params.gamma", 500);
-user_pref("gfx.font_rendering.cleartype_params.pixel_structure", 1);
-user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);
-user_pref("gfx.font_rendering.directwrite.enabled", true);
-user_pref("gfx.use_text_smoothing_setting", true);
-user_pref("gfx.font_rendering.directwrite.enabled", true);
-user_pref("layers.acceleration.disabled", false);
-user_pref("layers.acceleration.force-enabled", true);
