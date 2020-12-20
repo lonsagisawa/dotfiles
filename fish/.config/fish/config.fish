753ec6d2a291380276dca3f594a161244e491352
Lon Sagisawa <lon@sagisawa.me>
2020-12-20 14:33:44 +0900

    mac fish: more aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,14 +7,22 @@ set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH /opt/homebrew/bin $PATH
 
 # aliases
-# alias vim="nvim"
+## Neovim
+alias vim="nvim"
+## git alias
 alias g="git"
 alias gs="git status"
 alias gd="git diff"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+## replace ls for exa
 alias ls="exa -l"
 alias la="exa -la"
+## homebrew workaround
 alias brew="/usr/local/Homebrew/bin/brew"
 alias brew_arm="arch -arch arm64e /opt/homebrew/bin/brew"
+## VSC
 alias code="/usr/local/bin/code-insiders"
 
 # fisher
