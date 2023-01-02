0d4fb494f7c18ba51cc9eb7b9da1fd96f857acd5
Lon Sagisawa <lon@sagisawa.me>
2023-01-03 02:29:53 +0900

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,28 +1,41 @@
 local wezterm = require "wezterm"
+local wsl_domains = wezterm.default_wsl_domains()
 
-function default_shell()
-	if wezterm.target_triple == "aarch64-apple-darwin" then
-		return { "/opt/homebrew/bin/zsh", "-l" }
-	elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
-		return { "C:/Windows/system32/wsl.exe" }
-	end
+function font_size()
+  if wezterm.target_triple == "aarch64-apple-darwin" then
+    return 14
+  elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    return 10
+  end
 end
 
-
+for _, dom in ipairs(wsl_domains) do
+  dom.default_cwd = "~"
+end
 
 return {
-	window_padding = {
-		left = "16px",
-		right = "16px",
-		top = "16px",
-		bottom = "16px",
-	},
-	default_prog = default_shell(),
-	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	tab_bar_at_bottom = true,
-	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font "UDEV Gothic NFLG",
-	font_size = 14.0,
+  window_padding = {
+    left = "16px",
+    right = "16px",
+    top = "16px",
+    bottom = "16px",
+  },
+  keys = {
+    {
+      key = "j",
+      mods = "CTRL",
+      action = wezterm.action.Nop,
+    },
+  },
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
+  enable_scroll_bar = true,
+  color_scheme = "Catppuccin Mocha",
+  font = wezterm.font("UDEV Gothic NFLG"),
+  font_size = font_size(),
+  use_ime = true,
+  wsl_domains = wsl_domains,
+  default_domain = "WSL:Arch",
+  exit_behavior = "Close",
 }
 
