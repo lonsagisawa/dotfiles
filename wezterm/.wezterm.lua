34e8c1957f41648caee157548f7697d8bd240094
Lon Sagisawa <lon@sagisawa.me>
2023-01-03 21:09:38 +0900

    wezterm: comments

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,33 +1,35 @@
 local wezterm = require "wezterm"
 local wsl_domains = wezterm.default_wsl_domains()
 
+-- OS specific - Font size
 function font_size()
   if wezterm.target_triple == "aarch64-apple-darwin" then
+    -- macOS
     return 14
   elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    return 10
+    -- Windows
+    return 11
   end
 end
 
+-- OS specific - Default domain
 function default_domain()
   if wezterm.target_triple == "aarch64-apple-darwin" then
+    -- macOS
     return "local"
   elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    return "WSL:Arch"
+    -- Windows
+    return "WSL:Ubuntu"
   end
 end
 
+-- WSL - Default work directory
 for _, dom in ipairs(wsl_domains) do
   dom.default_cwd = "~"
 end
 
 return {
-  window_padding = {
-    left = "16px",
-    right = "16px",
-    top = "16px",
-    bottom = "16px",
-  },
+  -- keybind
   keys = {
     {
       key = "j",
@@ -35,15 +37,27 @@ return {
       action = wezterm.action.Nop,
     },
   },
+  -- window padding
+  window_padding = {
+    left = "16px",
+    right = "16px",
+    top = "16px",
+    bottom = "16px",
+  },
+  -- tabbar
   use_fancy_tab_bar = false,
   tab_bar_at_bottom = true,
-  enable_scroll_bar = true,
+  tab_max_width = 48,
+  -- color scheme
   color_scheme = "Catppuccin Mocha",
+  -- font
   font = wezterm.font("UDEV Gothic NFLG"),
   font_size = font_size(),
+  -- ime
   use_ime = true,
+  -- WSL specific
   wsl_domains = wsl_domains,
   default_domain = default_domain(),
+  -- behavior
   exit_behavior = "Close",
 }
-
