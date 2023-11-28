0a8e1ce436406d427d6ecd4e20920b08f61184b9
Lon Sagisawa <lon@sagisawa.me>
2023-11-28 23:04:55 +0900

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ local wsl_domains = wezterm.default_wsl_domains()
 function font_size()
   if wezterm.target_triple == "aarch64-apple-darwin" then
     -- macOS
-    return 14
+    return 15
   elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
     -- Windows
     return 11
@@ -39,22 +39,26 @@ return {
   },
   -- window padding
   window_padding = {
-    left = "16px",
-    right = "16px",
-    top = "16px",
-    bottom = "16px",
+    left = "8px",
+    right = "8px",
+    top = "8px",
+    bottom = "8px",
   },
   -- tabbar
   use_fancy_tab_bar = false,
-  tab_bar_at_bottom = true,
+  tab_bar_at_bottom = false,
   tab_max_width = 48,
   -- color scheme
   color_scheme = "Catppuccin Mocha",
   -- font
   font = wezterm.font("UDEV Gothic NFLG"),
   font_size = font_size(),
+  freetype_load_flags = "NO_HINTING",
+  line_height = 1.25,
   -- ime
   use_ime = true,
+  -- window border
+  window_decorations = "RESIZE",
   -- WSL specific
   wsl_domains = wsl_domains,
   default_domain = default_domain(),
