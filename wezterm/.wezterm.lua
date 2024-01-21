a2d028651f7e3fa6bc8424fb698bac9b3ef3e579
Lon Sagisawa <lon@sagisawa.me>
2024-01-21 22:56:30 +0900

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local wsl_domains = wezterm.default_wsl_domains()
 
-function isWindows()
+function IsWindows()
   if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     return true
   else
@@ -9,7 +9,7 @@ function isWindows()
   end
 end
 
-function isMac()
+function IsMac()
   if wezterm.target_triple == "aarch64-apple-darwin" then
     return true
   else
@@ -18,29 +18,29 @@ function isMac()
 end
 
 -- OS specific - Font size
-function fontSize()
-  if isMac() == true then
+function FontSize()
+  if IsMac() then
     return 14
   else
-    return 11
+    return 10.5
   end
 end
 
 -- OS specific - Default domain
-function defaultDomain()
-  if isWindows() == true then
+function WslDefaultDomain()
+  if IsWindows() then
     -- Windows
     return "WSL:Debian"
-  else 
+  else
     return "local"
   end
 end
 
 -- OS specific - Windows decoration
-function windowDecorations()
-  if isWindows() == true then
+function WindowDecorations()
+  if IsWindows() then
     return "INTEGRATED_BUTTONS|RESIZE"
-  else 
+  else
     return "TITLE|RESIZE"
   end
 end
@@ -69,23 +69,31 @@ return {
     inactive_titlebar_bg = "#181825",
   },
   -- tabbar
-  use_fancy_tab_bar = isWindows(),
+  use_fancy_tab_bar = IsWindows(),
   tab_bar_at_bottom = false,
   tab_max_width = 48,
   -- color scheme
   color_scheme = "Catppuccin Mocha",
   -- font
-  font = wezterm.font_with_fallback { "CaskaydiaCove NF", "LINE Seed JP_TTF" },
-  font_size = fontSize(),
+  font = wezterm.font_with_fallback {
+    {
+      family = "Fira Code",
+      weight = 450,
+      harfbuzz_features = { "cv02", "cv10", "ss01", "cv14" },
+    },
+    "LINE Seed JP_TTF",
+    "Symbols Nerd Font",
+  },
+  font_size = FontSize(),
   freetype_load_flags = "NO_HINTING",
-  line_height = 1.4,
+  line_height = 1.6,
   -- ime
   use_ime = true,
   -- window border
-  window_decorations = windowDecorations(),
+  window_decorations = WindowDecorations(),
   -- WSL specific
   wsl_domains = wsl_domains,
-  default_domain = defaultDomain(),
+  default_domain = WslDefaultDomain(),
   -- behavior
   exit_behavior = "Close",
   front_end = "WebGpu",
